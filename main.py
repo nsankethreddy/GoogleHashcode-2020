c4abd22b27514375b0c9bb6aef623dcce3980bd6
Abhishek262 <abhishek262014@gmail.com>
2020-02-21 02:39:17 +0530

    algo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-input_file = "input/a_example.txt"
+input_file = "input/b_read_on.txt"
 
 #stores 
 with open(input_file,"r") as fileObj:
@@ -21,10 +21,10 @@ with open(input_file,"r") as fileObj:
     #stores number of books , signup process time in days, and the num of books lib can ship per day
     libraries = []
     a = fileObj.readlines()
-    print(a)
+    # print(a)
     x = []
     y =[]
-    for i in range(len(a)):
+    for i in range(len(a)-1):
         if(i%2==0):
             x = a[i].split()
             x_int = [int(i/2), int(x[0]), int(x[1]), int(x[2])]
@@ -36,19 +36,44 @@ with open(input_file,"r") as fileObj:
                 y.append(int(i))
             books.append(y)
 
-    print(libraries)
-    print(books)
-    print(scores)
+
+    # print(libraries)
+    # print(books)
+    # print(scores)
 
 def algorithm(book_num,lib_num,scan_days,libraries,books,book_scores) : 
     #only book_num of books can be sent
     #book_num is length of book_scores
     #get total book score for all books in each library, order by them
     # print(libraries)
-    lib_old = libraries
-    libraries.sort(key = book_score_func)
-    print(libraries)
+    lib_old1 = libraries
+    lib_old2 = libraries
 
+    libraries.sort(key = book_score_func)
+    lib_sorted_score = libraries
+    lib_old1.sort(key = signup_func)
+    lib_sorted_signup = lib_old1
+    # print(lib_sorted_score)
+    # print(lib_sorted_signup)
+
+    #get half of the libraries based on signup time in asc
+    lib_sorted_signup_half = []
+    for i in range(0,len(lib_sorted_signup)/2) : 
+        lib_sorted_signup_half.append(lib_sorted_signup[i])
+    
+    lib_sorted_signup_half.sort(key = signup_density,reverse = True)
+    # print(lib_sorted_signup_half)
+
+
+    #lib_index will be ordered by signup density factor
+    for i in range(0,len(lib_sorted_signup_half)):
+        op_libs.append([lib_sorted_signup_half[0],2])
+        for i in range(0,2):
+            
+            
+
+
+        op_books.append()
 
 
     #returns a list of lists op_libs containing lib_index, num of books for scanning. op_books contains books sent for scanning
@@ -57,10 +82,17 @@ def algorithm(book_num,lib_num,scan_days,libraries,books,book_scores) :
 #takes every element in library as argument
 def book_score_func(elem) : 
     l = [int(scores[i]) for i in books[elem[0]]]
-    print(sum(l))
+    # print(sum(l))
     return sum(l)
 
+def signup_func(elem):
+    return elem[2]
 
+def signup_density(elem) : 
+    l = [int(scores[i]) for i in books[elem[0]]]
+    m = sum(l)*elem[3]/elem[2]
+    return m
+    # m = m/
 
 def write(filename,num_libs,libraries,books): 
     with open(filename,'w') as fileObj : 
